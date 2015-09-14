46bcdaec11ebc52c2d61ba1de2c3c2f3ef92c810
Gerson Lázaro <gersonlazaro@gersonlazaro.com>
2015-09-14 16:14:02 -0500

    Corrección en la prueba en php del registro de asistencia: Ahora ignora espacios vacios

--- vistas/ejemplo_validar_asistencia_curso.php
+++ vistas/ejemplo_validar_asistencia_curso.php
@@ -1,8 +1,16 @@
+
 <?php
+
+/*Cada $cod[i] guarda un código recibido, Al recorrer el array debe hacerse el if que contiene el strcpm para evitar añadir 
+espacios vacios
+.*/
 if (isset($_POST['codigo'])) {
     $cod = $_POST['codigo'];
     for ($i=0; $i<count($cod); $i++) {
-        echo $cod[$i]."<br />";
+    	if(strcmp($cod[$i], "")!==0){
+    		echo $cod[$i]."<br />";
+    	}
+        
     }
 }
 ?>
