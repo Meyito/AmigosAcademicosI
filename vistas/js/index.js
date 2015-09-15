779de86e4f3cf330cd98d0bf1ac1ef10ee6d678d
Gerson Lázaro <gersonlazaro@gersonlazaro.com>
2015-09-14 21:25:53 -0500

    Añadida vista de inicio para el amigo academico

--- vistas/js/index.js
+++ vistas/js/index.js
@@ -137,6 +137,3 @@ function copiarTextArea(){
 
 /*Escuchadores*/
 window.addEventListener("resize", sidebarAdjust, false);
-document.getElementById("tipoComentario1").addEventListener("click", cambiarTipoComentario, false);
-document.getElementById("tipoComentario2").addEventListener("click", cambiarTipoComentario, false);
-document.getElementById("0").addEventListener("keyup", copiarTextArea, false);
