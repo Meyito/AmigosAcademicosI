1ae0fe3fbdc2a03334dd8b13e799f121cc020205
danieldvs95 <daniel_dvs95@hotmail.com>
2015-09-16 18:12:33 -0500

    Se añadió query para registrar al Estudiante
    Método que realiza la inserción de un estudiante en la base de datos

---
+++ App/Core/Model/studentDB.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once "Core/Model/model.php";
+
+class studentDB extends Model{
+
+	public function addStudent($id,$password,$name,$semester,$email,$avatar){
+		$this->connect();
+		$query = $this->query("INSERT INTO Usuario('".$id."','".$name."','".$password."','".$email."',".$semester.",3,'".$avatar."','activo')");
+		$this->terminate();
+		return $query;
+	}
+}
+
+?>
